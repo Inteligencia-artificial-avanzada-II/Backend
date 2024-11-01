b89583aaabbdfb4d33a4e75c7bff4f28fd8a961d
A01754412 <88682649+A01754412@users.noreply.github.com>
2024-11-01 12:38:23 -0600

    Cambios respuesta Json

--- src/controllers/OrdenController.ts
+++ src/controllers/OrdenController.ts
@@ -26,10 +26,10 @@ class OrdenController extends AbstractController {
 
   private async getTest(req: Request, res: Response) {
     /**
-    * Prueba de conexión con el controlador
-    * @param - None
-    * @returns - None
-    */
+     * Prueba de conexión con el controlador
+     * @param - None
+     * @returns - None
+     */
     try {
       res.status(200).send("Orden Works");
     } catch (error) {
@@ -42,37 +42,70 @@ class OrdenController extends AbstractController {
       const { sqlData, mongoData } = req.body;
 
       // Validaciones de sqlData
-      const sqlRequiredFields = ["idContenedor", "idCamion", "origen", "idCedis"];
+      const sqlRequiredFields = [
+        "idContenedor",
+        "idCamion",
+        "origen",
+        "idCedis",
+      ];
       for (const field of sqlRequiredFields) {
         if (!sqlData || !sqlData[field]) {
-          return res.status(400).send(`El campo ${field} es obligatorio en sqlData.`);
+          return res
+            .status(400)
+            .send(`El campo ${field} es obligatorio en sqlData.`);
         }
       }
 
       // Validaciones de mongoData
-      const mongoRequiredFields = ["orderNumber", "createdBy", "modifiedBy", "creationDate", "products"];
+      const mongoRequiredFields = [
+        "orderNumber",
+        "createdBy",
+        "modifiedBy",
+        "creationDate",
+        "products",
+      ];
       for (const field of mongoRequiredFields) {
         if (!mongoData || !mongoData[field]) {
-          return res.status(400).send(`El campo ${field} es obligatorio en mongoData.`);
+          return res
+            .status(400)
+            .send(`El campo ${field} es obligatorio en mongoData.`);
         }
       }
 
       // Validación adicional para los productos en mongoData
-      if (!Array.isArray(mongoData.products) || mongoData.products.length === 0) {
-        return res.status(400).send("El campo products debe ser un arreglo no vacío en mongoData.");
+      if (
+        !Array.isArray(mongoData.products) ||
+        mongoData.products.length === 0
+      ) {
+        return res
+          .status(400)
+          .send("El campo products debe ser un arreglo no vacío en mongoData.");
       }
 
       // Validaciones para cada producto dentro del arreglo products
       const productRequiredFields = [
-        "itemCode", "itemDescription", "originalOrderQuantity", "requestedQuantity", "assignedQuantity",
-        "packedQuantity", "orderDetailStatus", "expirationDateComprobante", "barcode", "salePrice",
-        "alternativeItemCodes", "unitOfMeasure"
+        "itemCode",
+        "itemDescription",
+        "originalOrderQuantity",
+        "requestedQuantity",
+        "assignedQuantity",
+        "packedQuantity",
+        "orderDetailStatus",
+        "expirationDateComprobante",
+        "barcode",
+        "salePrice",
+        "alternativeItemCodes",
+        "unitOfMeasure",
       ];
 
       for (const product of mongoData.products) {
         for (const field of productRequiredFields) {
           if (!product[field]) {
-            return res.status(400).send(`El campo ${field} es obligatorio en cada producto de mongoData.products.`);
+            return res
+              .status(400)
+              .send(
+                `El campo ${field} es obligatorio en cada producto de mongoData.products.`
+              );
           }
         }
       }
@@ -88,13 +121,13 @@ class OrdenController extends AbstractController {
       // Crear la orden en SQL e incluir el idMongoProductos
       const orden = await db.Orden.create({
         ...sqlData,
-        idMongoProductos: mongoOrder._id.toString() // Guardamos el ID de MongoDB en SQL
+        idMongoProductos: mongoOrder._id.toString(), // Guardamos el ID de MongoDB en SQL
       });
 
       // Respuesta con las órdenes creadas en SQL y MongoDB
       res.status(201).send({
         message: "Orden generada exitosamente",
-        data: { sqlOrden: orden, mongoOrden: mongoOrder }
+        data: { sqlOrden: orden, mongoOrden: mongoOrder },
       });
     } catch (error) {
       res.status(500).send(`Error al crear la Orden: ${error}`);
@@ -132,18 +165,27 @@ class OrdenController extends AbstractController {
       const orden = await db.Orden.findByPk(id);
 
       const mongoController = MongoProductosController.instance;
-      const ordenMongo = await mongoController.getOrderById(orden.idMongoProductos)
+      const ordenMongo = await mongoController.getOrderById(
+        orden.idMongoProductos
+      );
 
       if (!orden) {
-        res.status(404).send(`Orden con id ${id} no encontrada`);
+        res
+          .status(404)
+          .json({ message: `Orden con id ${id} no encontrada`, data: {} });
         return;
       }
       res.status(200).json({
-        sqlData: orden,
-        mongoData: ordenMongo
+        message: "Datos obtenidos exitosamente",
+        data: {
+          sqlData: orden,
+          mongoData: ordenMongo,
+        },
       });
     } catch (error) {
-      res.status(500).send(`Error al consultar la Orden: ${error}`);
+      res
+        .status(500)
+        .json({ message: `Error al consultar la Orden: ${error}`, data: {} });
     }
   }
 
